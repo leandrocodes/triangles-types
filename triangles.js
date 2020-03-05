0eb083482a5134e458b1ada1331007d952794a92
leandrocodes <leandrovianacodes@gmail.com>
2020-03-05 19:17:23 -0400

    verificação de erros

--- triangles.js
+++ triangles.js
@@ -6,11 +6,22 @@ const result = document.getElementById('result')
 
 btn.addEventListener('click', () => {
   console.log('Calculando...')
+  if (!isNaN(side1.value) && !isNaN(side2.value) && !isNaN(side3.value)) {
+    if (side1.value <= 0 || side2.value <= 0 || side3.value <= 0) {
+      result.classList.add('error')
+      result.innerText = 'Erro! Não pode número menores que zero!'
+    } else {
+      if (side1.value === side2.value && side1.value === side3.value && side2.value === side3.value)
+        result.innerText = 'O trinângulo é equilátero'
 
-  if (side1.value === side2.value && side1.value === side3.value && side2.value === side3.value)
-    console.log('esquilátero')
-  if (side1.value === side2.value || side2.value === side3.value || side1.value === side3.value)
-    console.log('isóceles')
-  if (side1.value !== side2.value && side1.value !== side3.value) console.log('escaleno')
-  else console.log('triangulo inválido!')
+      if (side1.value === side2.value || side2.value === side3.value || side1.value === side3.value)
+        result.innerText = 'O triângulo é isóceles'
+
+      if (side1.value !== side2.value && side1.value !== side3.value && side2.value !== side3.value)
+        result.innerText = 'O triângulo é escaleno'
+    }
+  } else {
+    result.innerText = 'Somente caracteres são válidos'
+    result.classList.add('error')
+  }
 })
